f739472fc81627bfaacdaa2bbf9f8e0f4c0ac8fc
Hernán Vega <hervega123@gmail.com>
2024-07-17 03:46:01 -0300

    Integrated: enemy model

---
+++ scenes/Area2D.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+func _on_area_entered(area):
+	pass # Replace with function body.
--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -1,1 +1,69 @@
 extends CharacterBody2D
+class_name npc
+
+@onready var hit_box = $"hit_box/area_guf"
+@onready var animation = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+@onready var reposo = $"../reposo"
+@onready var action = $"../action"
+
+var player = null
+var run = false
+var idle = false
+var move = Vector2.ZERO
+var speed = 10
+
+func _ready():
+	movement()
+	
+
+
+
+func _physics_process(delta):
+	
+	
+	move_and_slide()
+	if run == false  && idle == false:
+		animation.play("mary_walk")
+
+func movement():
+	reposo.start()
+	velocity.x = speed
+	
+	
+
+
+func _on_hit_box_area_entered(area):
+	run = true
+	set_collision_mask_value(1,false)
+	animation.play("mary_run")
+	
+	if $up.is_colliding():
+		velocity.x = -speed * 10
+		animation.flip_h = true
+	elif $down.is_colliding():
+		velocity.x = -speed * 10
+		animation.flip_h = true
+	else:
+		if $rigth.is_colliding():
+			velocity.x = -speed * 10
+			animation.flip_h = true
+		elif $left.is_colliding():
+			velocity.x = speed * 10
+	
+	
+
+
+func _on_reposo_timeout():
+	if run == false:
+		idle = true
+		velocity.x = 0
+		animation.play("mary_idle")
+		action.start()
+	
+
+
+func _on_action_timeout():
+	if run == false:
+		idle = false
+		movement()
