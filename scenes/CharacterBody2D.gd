4d781995526209b107551c14421a334dc0d98f77
Hernán Vega <hervega123@gmail.com>
2024-07-17 20:33:48 -0300

    Integrated: improvements in npcs mechanics

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -6,6 +6,7 @@ class_name npc
 @onready var collision = $CollisionShape2D
 @onready var reposo = $"../reposo"
 @onready var action = $"../action"
+@onready var mary = $"."
 
 var player = null
 var run = false
@@ -15,29 +16,28 @@ var speed = 10
 
 func _ready():
 	movement()
-	
-
-
 
 func _physics_process(delta):
+	
+	out_of_screen()
 	
 	
 	move_and_slide()
 	if run == false  && idle == false:
 		animation.play("mary_walk")
 
+		
+
 func movement():
 	reposo.start()
 	velocity.x = speed
-	
-	
-
+	if velocity.x < 0:
+		animation.flip_h
 
 func _on_hit_box_area_entered(area):
 	run = true
 	set_collision_mask_value(1,false)
 	animation.play("mary_run")
-	
 	if $up.is_colliding():
 		velocity.x = -speed * 10
 		animation.flip_h = true
@@ -51,9 +51,6 @@ func _on_hit_box_area_entered(area):
 		elif $left.is_colliding():
 			velocity.x = speed * 10
 	
-	
-
-
 func _on_reposo_timeout():
 	if run == false:
 		idle = true
@@ -61,9 +58,14 @@ func _on_reposo_timeout():
 		animation.play("mary_idle")
 		action.start()
 	
-
-
 func _on_action_timeout():
 	if run == false:
 		idle = false
 		movement()
+
+func out_of_screen():
+	if (global_position.x <= -250) || (global_position.x >= 500):
+		self.queue_free()
+	elif (global_position.y <= 10) || (global_position.y >= 400):
+		self.queue_free()
+	
